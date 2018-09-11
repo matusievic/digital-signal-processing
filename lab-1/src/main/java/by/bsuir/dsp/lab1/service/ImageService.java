3a24e5b5b3a2dff23660ba0c1dd8f2a6e89ad695
Andrei Matusevich <matusievic@gmail.com>
2018-09-12 00:21:07 +0300

    Add Prewitt operator

--- lab-1/src/main/java/by/bsuir/dsp/lab1/service/ImageService.java
+++ lab-1/src/main/java/by/bsuir/dsp/lab1/service/ImageService.java
@@ -66,7 +66,6 @@ public class ImageService {
         return result;
     }
 
-
     private static int getDissected(int intensity, int f, int g, int mode) {
         switch (mode) {
             case 0:
@@ -84,5 +83,80 @@ public class ImageService {
             default:
                 throw new IllegalArgumentException("Please provide a valid mode");
         }
+    }
+
+    public static BufferedImage prewitt(BufferedImage image) {
+        final int[][] pCore = {
+                {1, 0, -1},
+                {1, 0, -1},
+                {1, 0, -1}
+        };
+
+        final int[][] qCore = {
+                {-1, -1, -1},
+                {0, 0, 0},
+                {1, 1, 1}
+        };
+
+        BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+        for (int i = 1; i < image.getWidth() - 1; i++) {
+            for (int j = 1; j < image.getHeight() - 1; j++) {
+                int[][] currentRed = {
+                        {new Color(image.getRGB(i - 1, j - 1)).getRed(), new Color(image.getRGB(i - 1, j)).getRed(), new Color(image.getRGB(i - 1, j + 1)).getRed()},
+                        {new Color(image.getRGB(i, j - 1)).getRed(), new Color(image.getRGB(i, j)).getRed(), new Color(image.getRGB(i, j + 1)).getRed()},
+                        {new Color(image.getRGB(i + 1, j - 1)).getRed(), new Color(image.getRGB(i + 1, j)).getRed(), new Color(image.getRGB(i + 1, j + 1)).getRed()}
+                };
+
+                int red = Math.max(calculateProperty(pCore, currentRed), calculateProperty(qCore, currentRed));
+                if (red > 255) {
+                    red = 255;
+                } else if (red < 0) {
+                    red = 0;
+                }
+
+                int[][] currentGreen = {
+                        {new Color(image.getRGB(i - 1, j - 1)).getGreen(), new Color(image.getRGB(i - 1, j)).getGreen(), new Color(image.getRGB(i - 1, j + 1)).getGreen()},
+                        {new Color(image.getRGB(i, j - 1)).getGreen(), new Color(image.getRGB(i, j)).getGreen(), new Color(image.getRGB(i, j + 1)).getGreen()},
+                        {new Color(image.getRGB(i + 1, j - 1)).getGreen(), new Color(image.getRGB(i + 1, j)).getGreen(), new Color(image.getRGB(i + 1, j + 1)).getGreen()}
+                };
+
+                int green = Math.max(calculateProperty(pCore, currentGreen), calculateProperty(qCore, currentGreen));
+                if (green > 255) {
+                    green = 255;
+                } else if (green < 0) {
+                    green = 0;
+                }
+
+                int[][] currentBlue = {
+                        {new Color(image.getRGB(i - 1, j - 1)).getBlue(), new Color(image.getRGB(i - 1, j)).getBlue(), new Color(image.getRGB(i - 1, j + 1)).getBlue()},
+                        {new Color(image.getRGB(i, j - 1)).getBlue(), new Color(image.getRGB(i, j)).getBlue(), new Color(image.getRGB(i, j + 1)).getBlue()},
+                        {new Color(image.getRGB(i + 1, j - 1)).getBlue(), new Color(image.getRGB(i + 1, j)).getBlue(), new Color(image.getRGB(i + 1, j + 1)).getBlue()}
+                };
+
+                int blue = Math.max(calculateProperty(pCore, currentBlue), calculateProperty(qCore, currentBlue));
+                if (blue > 255) {
+                    blue = 255;
+                } else if (blue < 0) {
+                    blue = 0;
+                }
+
+                result.setRGB(i, j, new Color(red, green, blue).getRGB());
+            }
+        }
+
+        return result;
+    }
+
+    private static int calculateProperty(int[][] core, int[][] current) {
+        int accumulator = 0;
+
+        for (int i = 0; i < core.length; i++) {
+            for (int j = 0; j < core.length; j++) {
+                accumulator += core[i][j] * current[i][j];
+            }
+        }
+
+        return accumulator;
     }
 }
