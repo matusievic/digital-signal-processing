daffb05f7b3b540cf81cc314998b64916988e674
Andrei Matusevich <matusievic@gmail.com>
2018-09-11 23:15:57 +0300

    Add dissection

--- lab-1/src/main/java/by/bsuir/dsp/lab1/service/ImageService.java
+++ lab-1/src/main/java/by/bsuir/dsp/lab1/service/ImageService.java
@@ -48,4 +48,41 @@ public class ImageService {
 
         return result;
     }
+
+    public static BufferedImage dissect(BufferedImage image, int f, int g, int mode) {
+        BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_ARGB);
+
+        for (int i = 0; i < image.getWidth(); i++) {
+            for (int j = 0; j < image.getHeight(); j++) {
+                Color oldColor = new Color(image.getRGB(i, j));
+                int newR = getDissected(oldColor.getRed(), f, g, mode);
+                int newG = getDissected(oldColor.getGreen(), f, g, mode);
+                int newB = getDissected(oldColor.getBlue(), f, g, mode);
+                Color newColor = new Color(newR, newG, newB);
+                result.setRGB(i, j, newColor.getRGB());
+            }
+        }
+
+        return result;
+    }
+
+
+    private static int getDissected(int intensity, int f, int g, int mode) {
+        switch (mode) {
+            case 0:
+                if (intensity < f) {
+                    return 0;
+                } else {
+                    return (g * (intensity - f)) / (255 - f);
+                }
+            case 1:
+                if (intensity > f) {
+                    return 255;
+                } else {
+                    return (intensity * (255 - g)) / f + g;
+                }
+            default:
+                throw new IllegalArgumentException("Please provide a valid mode");
+        }
+    }
 }
