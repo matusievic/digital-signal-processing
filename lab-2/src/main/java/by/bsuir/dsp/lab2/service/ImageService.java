222b21ccc53cfd9990e06cd70909636195eddbd9
Andrei Matusevich <matusievic@gmail.com>
2018-09-19 19:19:18 +0300

    Implement object detection

--- lab-2/src/main/java/by/bsuir/dsp/lab2/service/ImageService.java
+++ lab-2/src/main/java/by/bsuir/dsp/lab2/service/ImageService.java
@@ -2,6 +2,11 @@ package by.bsuir.dsp.lab2.service;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public final class ImageService {
     public static BufferedImage toGrayscaled(BufferedImage image) {
@@ -80,8 +85,12 @@ public final class ImageService {
     public static int[] getHistogram(BufferedImage image, char key) {
         int[] result = new int[256];
 
-        for (int i = 0; i < image.getWidth(); i++) {
-            for (int j = 0; j < image.getHeight(); j++) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+        int n = width * height;
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
                 int index;
                 switch (key) {
                     case 'r':
@@ -97,6 +106,88 @@ public final class ImageService {
                         throw new IllegalArgumentException("Please provide a valid key");
                 }
                 result[index]++;
+            }
+        }
+
+        return result;
+    }
+
+    public static BufferedImage mark(BufferedImage image) {
+        BufferedImage result = new BufferedImage(image.getWidth(), image.getHeight(), BufferedImage.TYPE_INT_RGB);
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        int black = new Color(0, 0, 0).getRGB();
+        int white = new Color(255, 255, 255).getRGB();
+        int label = 0;
+
+        int[][] map = new int[width][height];
+
+        List<Integer> unusedLabels = new ArrayList<>();
+
+        for (int i = 1; i < width; i++) {
+            for (int j = 1; j < height; j++) {
+                boolean x1 = map[i - 1][j] != 0 && map[i][j - 1] == 0;
+                boolean x2 = map[i - 1][j] == 0 && map[i][j - 1] != 0;
+
+                if (image.getRGB(i, j) == black) {
+                    continue;
+                } else if (map[i - 1][j] == 0 && map[i][j - 1] == 0) {
+                    label++;
+                    map[i][j] = label;
+                } else if (x1 || x2) {
+                    if (x1) {
+                        map[i][j] = map[i - 1][j];
+                    } else {
+                        map[i][j] = map[i][j - 1];
+                    }
+                } else if (map[i - 1][j] != 0 && map[i][j - 1] != 0) {
+                    if (map[i - 1][j] == map[i][j - 1]) {
+                        map[i][j] = map[i - 1][j];
+                    } else {
+                        int cMark = map[i][j - 1];
+                        int bMark = map[i - 1][j];
+
+                        for (int i0 = 1; i0 < width; i0++) {
+                            for (int j0 = 1; j0 < height; j0++) {
+                                if (map[i0][j0] == cMark) {
+                                    map[i0][j0] = bMark;
+                                }
+                            }
+                        }
+
+                        unusedLabels.add(cMark);
+                        map[i][j] = bMark;
+                    }
+                }
+            }
+        }
+
+        Color[] colors = {Color.BLUE, Color.CYAN, Color.YELLOW, Color.RED, Color.GREEN, Color.MAGENTA, Color.ORANGE, Color.PINK, Color.LIGHT_GRAY, Color.GRAY};
+
+        List<Integer> labels = new ArrayList<>(label);
+        for (int i = 1; i <= label; i++) {
+            labels.add(i);
+        }
+        labels.removeAll(unusedLabels);
+
+        Random r = new Random(255);
+        Map<Integer, Color> colorMap = new HashMap<>(labels.size());
+        for (int i = 0; i < labels.size(); i++) {
+            Color color = null;
+            if (i < colors.length) {
+                color = colors[i];
+            } else {
+                color = new Color(r.nextInt(255), r.nextInt(255), r.nextInt(255));
+            }
+            colorMap.put(labels.get(i), color);
+        }
+
+        for (int i = 0; i < width - 1; i++) {
+            for (int j = 0; j < height - 1; j++) {
+                Color color = colorMap.getOrDefault(map[i][j], Color.BLACK);
+                result.setRGB(i, j, color.getRGB());
             }
         }
 
